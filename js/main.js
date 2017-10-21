bf5956864cf3db43f7011db1e7846e5c4a587d63
HalilCan <halilcanmemoglu@gmail.com>
2017-10-21 19:57:39 -0400

    POST request

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// You REALLY want async = true.
+// Otherwise, it'll block ALL execution waiting for server response.
+let async = true;
+
 let imeiBox = document.getElementById("imei-box");
 let usernameBox = document.getElementById("username-box");
 let passwordBox = document.getElementById("password-box");
@@ -15,27 +19,50 @@ let IMEI = "";
 let USERNAME = "";
 let PASSWORD = "";
 
+let msgUrl = "https://core.rock7.com/rockblock/MT";
+let method = "POST";
+let postData = "";
+
 function sendMessage() {
     let message = sendMessageBox.value;
     let hexMessage = hexify(message);
-    
     let messageObject = {
         imei: IMEI,
         username: USERNAME,
         password: PASSWORD,
         data: hexMessage
-    }
+    };
     let preppedObject = JSON.stringify(messageObject);
     
-    let xhr = new XMLHttpRequest();
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    method = "POST";
+    postData = preppedObject;
     
-    // send the collected data as JSON
-    xhr.send(JSON.stringify(data));
-    
-    xhr.onloadend = function () {
-        // done
+    let request = new XMLHttpRequest();
+    // Before we send anything, we first have to say what we will do when the
+    // server responds. This seems backwards (say how we'll respond before we send
+    // the request? huh?), but that's how Javascript works.
+    // This function attached to the XMLHttpRequest "onload" property specifies how
+    // the HTTP response will be handled.
+    request.onload = function () {
+        // Because of javascript's fabulous closure concept, the XMLHttpRequest "request"
+        // object declared above is available in this function even though this function
+        // executes long after the request is sent and long after this function is
+        // instantiated. This fact is CRUCIAL to the workings of XHR in ordinary
+        // applications.
+        
+        let status = request.status;
+        let data = request.responseText;
+        Console.log("Status: " + status);
+        Console.log("Return data: " + data);
     };
+    request.open(method, url, async);
+    
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // Or... request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    // Or... whatever
+    
+    // Actually sends the request to the server.
+    request.send(postData);
 }
 
 function hexify(val) {
